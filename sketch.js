0bdc981cc54e7060791473fc086d81b768559bdd
MoHiT7654321 <77192226+MoHiT7654321@users.noreply.github.com>
2021-02-10 22:52:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+var canvas;
+var block1, block2, block3, block4;
+var ball, edges;
+var music
+
+function preload(){
+  music = loadSound("music.mp3");
+}
+
+function setup(){
+  canvas = createSprite(800,800);
+
+  block1 = createSprite(0,580,360,30);
+  block1.shapeColor = ("lime");
+
+  block2 = createSprite(295,580,200,30);
+  block2.shapeColor = ("black");
+
+  block3 = createSprite(515,580,200,30);
+  block3.shapeColor = ("blue");
+
+  block4 = createSprite(740,580,220,30);
+  block4.shapeColor = ("yellow");
+
+  ball = createSprite(random(20,750),100,30,30);
+  ball.shapeColor = ("white")
+  ball.velocityX = 5;
+  ball.velocityY = 9;
+}
+
+function draw(){
+  background("red")
+  edges =  createEdgeSprites();
+  ball.bounceOff(edges);
+
+  if(block1.isTouching(ball) && ball.bounceOff(block1)){
+    ball.shapeColor = ("lime");
+    music.play();
+
+  }
+
+  if(block2.isTouching(ball)){
+    ball.shapeColor("black")
+    ball.velocityX = 0;
+    ball.velocityY = 0;
+    music.stop();
+
+  }
+
+  if(block3.isTouching(ball) && ball.bounceOff(block3)){
+    ball.shapeColor("blue")
+  }
+
+  if(block4.isTouching(ball) && ball.bounceOff(block4)){
+    ball.shapeColor("yellow")
+  }
+  drawSprites();
+}
